a59320659cfe094f9aca6dbb0823baf5d9aaf31a
Chen Peleg <chenpeleg@gmail.com>
2024-04-23 21:05:17 +0300

    service worker working

--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,36 @@
-
-
-
 // Change this to your repository name
-var GHPATH = '/Rotem-fortune';
+const GHPATH = '/Rotem-fortune';
 
 // Choose a different app prefix name
-var APP_PREFIX = 'rtmfort_';
+const APP_PREFIX = 'rtmfort_';
 
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…).
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_00';
+const VERSION = 'version_00';
 
 // The files to make available for offline use. make sure to add
 // others to this list
-var URLS = [
-    `${GHPATH}/`,
-    `${GHPATH}/index.html`,
-    // `${GHPATH}/css/styles.css`,
+const URLS = [`${GHPATH}/`, `${GHPATH}/index.html`, // `${GHPATH}/css/styles.css`,
     // `${GHPATH}/js/app.js`
-]
-self.addEventListener("install", event => {
-    console.log("Service worker installed");
+];
+const CACHE_STATIC_NAME = APP_PREFIX + VERSION;
+const CACHE_DYNAMIC_NAME = APP_PREFIX + 'dynamic';
+self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activating Service Worker ....', event);
+    event.waitUntil(caches.keys()
+        .then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                    console.log('[Service Worker] Removing old cache.', key);
+                    return caches.delete(key);
+                }
+            }));
+        }));
+    return self.clients.claim();
 });
-self.addEventListener("activate", event => {
-    console.log("Service worker activated");
+self.addEventListener('install', event => {
+    console.log('Service worker installed', event.type);
 });
+
