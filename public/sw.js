8f63068925df4ee51b6ab5e46d21c22a6b05a8cb
Chen Peleg <chenpeleg@gmail.com>
2024-05-01 11:39:26 +0300

    sw 2

--- public/sw.js
+++ public/sw.js
@@ -34,14 +34,14 @@ const clearCaches = async () => {
 };
 
 /**
- * @param { Event & {waitUntil : (Promise) =>Promise  }} activationEvent
- * @return {Promise<void>}
- */
+* @param { Event & {waitUntil : (Promise) =>Promise  }} activationEvent
+* @return {Promise<void>}
+*/
 const activateEventHandler = async (activationEvent) => {
     Debug.log(`[Service Worker] Activating Service Worker version ${swConstants.version} ....`);
     await activationEvent.waitUntil(clearCaches());
     return self.clients.claim();
-};
+}
 /**
  *
  * @return {Promise<void>}
@@ -72,15 +72,30 @@ const fetchRequestAndCache = async (request) => {
  * @return {Promise<FetchEvent>}
  */
 const fetchEventHandler = async (fetchEvent) => {
-    const cachedRequest = await (caches.match(fetchEvent.request));
-    const response = cachedRequest ? cachedRequest :
-       await fetchRequestAndCache(fetchEvent.request);
-    fetchEvent.respondWith(response);
-};
+    fetchEvent.respondWith(caches.match(fetchEvent.request).then((fetchResponse) => {
+        if (fetchResponse) {
+            Debug.debounceLog('[Service Worker] From cache: ' + fetchEvent.request.url);
+            return fetchResponse;
+        }
+        return fetch(fetchEvent.request).then((response) => {
+            return caches.open(swConstants.CACHE_NAME).then((cache) => {
+                if (!fetchEvent.request.url.includes('@')) {
+                    // don't cache in development mode (the @ is used for the vite
+                    // server files)
+                    Debug.log('[Service Worker] Caching resource: ' +
+                        fetchEvent.request.url);
+                    cache.put(fetchEvent.request, response.clone());
+                }
+
+                return response;
+            });
+        });
+    }));
+}
 
 
 self.addEventListener('activate', activateEventHandler);
 
 self.addEventListener('install', installEventHandler);
 
-self.addEventListener('fetch', fetchEventHandler);
+self.addEventListener('fetch',fetchEventHandler );
